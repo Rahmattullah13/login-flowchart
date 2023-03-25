35150b5b412a13b66bae69b28d0bb32e2083b432
Rahmattullah <rfkrhmttllh@gmail.com>
2023-03-25 11:21:31 +0700

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { DiagramModule } from '@syncfusion/ej2-angular-diagrams';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -8,6 +8,7 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
+    DiagramModule,
     BrowserModule
   ],
   providers: [],
